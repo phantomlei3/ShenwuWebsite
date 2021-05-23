3d30c10f792fdef6c8eb07d120f43ba659bf160e
Lei Luo <phantomlei3@gmail.com>
2021-05-23 15:03:03 +0800

    updated version 0.1

--- app.py
+++ app.py
@@ -1,29 +1,65 @@
 from flask import Flask, redirect, url_for, render_template, request, session
-
+import csv
 
 app = Flask(__name__)
+
+with open("info_data/public_info.csv", "r", encoding="utf-8") as f:
+    public_info = [{k: v for k, v in row.items()} for row in csv.DictReader(f, skipinitialspace=True)]
+
+public_info = sorted(public_info, key=lambda info: info['time'], reverse=True)
+info_id_dict = {info['id']: info for info in public_info}
+
+announce_info = [info for info in public_info if info['type'] == "公告"]
+event_info = [info for info in public_info if info['type'] == "活动"]
+news_info = [info for info in public_info if info['type'] == "新闻"]
 
 
 # 首页
 @app.route('/')
 @app.route('/index')
 def index():
-    return render_template("base.html", )
+
+    # visualization limitation: 10
+    return render_template("index.html", public_info=public_info[:10],
+                            announce_info=announce_info[:10], event_info=event_info[:10], news_info=news_info[:10])
 
 # 游戏公告
 @app.route('/announcements')
 def announcements():
-    return "announcements"
+    banner = "announcement"
+    return render_template("info.html", banner=banner, all_info=announce_info)
 
 # 活动专区
 @app.route('/events')
 def events():
-    return "events"
+    banner = "event"
+    return render_template("info.html", banner=banner, all_info=event_info)
 
-# 更新资讯
+# 新闻资讯
 @app.route('/news')
 def news():
-    return "news"
+    banner = "news"
+    return render_template("info.html", banner=banner, all_info=news_info)
+
+
+@app.route('/article', methods=['GET'])
+def article():
+    # 获取article id
+    id = str(request.args.get('id')).lower()
+    # 设置banner
+    if 'a' in id:
+        banner = "announcement"
+    elif 'e' in id:
+        banner = "event"
+    else:
+        banner = "news"
+
+    article = info_id_dict[id]
+
+    return render_template("article.html", banner=banner, article=article)
+
+
+
 
 # 神武图书馆
 @app.route('/wiki')
@@ -39,7 +75,7 @@ def infolibrary():
 # 游戏下载
 @app.route('/download')
 def download():
-    return 'download'
+    return render_template("download.html")
 
 
 if __name__ == '__main__':
